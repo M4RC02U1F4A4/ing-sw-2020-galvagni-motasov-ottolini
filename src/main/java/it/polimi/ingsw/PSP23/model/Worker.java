66db3e7adf4f48be6cacb16b2cda9599b42db8ab
Marco <23193188+Marco1097@users.noreply.github.com>
2020-04-10 11:10:12 +0200

    Worker Javadoc

--- src/main/java/it/polimi/ingsw/PSP23/model/Worker.java
+++ src/main/java/it/polimi/ingsw/PSP23/model/Worker.java
@@ -1,39 +1,74 @@
 package it.polimi.ingsw.PSP23.model;
-
+/**
+* Worker class
+*/
 public class Worker {
     private Cell cell;
     Color color;
 
-    //costruttore da usare quando si collocano i giocatori sulla mappa
+    /**
+    *   Costructor used when the worker are placed on the map
+    *   The color of the worker defines who it belogs to
+    *   @param c the cell in which the worker must be positioned
+    *   @param color the worker color
+    */
     public Worker(Cell c, Color color) {
         this.cell = c;
         this.color = color;
     }
 
+    /**
+    *   Set the worker color
+    *   @param color worker color 
+    */
     public void setColor(Color color) {
         this.color = color;
     }
 
+    /**
+    *   Return the worker color
+    *   @return worker color
+    */
     public Color getColor() {
         return color;
     }
 
+    /**
+    *   Call the function used to return position X on the map
+    *   @return the X position on the map
+    */
     public int getPosX() {
         return cell.getX();
     }
 
+    /**
+    *   Call the function used to return position Y on the map
+    *   @return Y position on the map
+    */
     public int getPosY() {
         return cell.getY();
     }
 
+    /**
+    *   Call the function used to return position Z on the map
+    *   @return Z position on the map
+    */
     public int getPosZ() {
         return cell.height();
     }
 
+    /**
+    *   Return the cell object
+    *   @return the cell object
+    */
     public Cell getCell() {
         return this.cell;
     }
 
+    /**
+    *   The worker is removed from the current cell and placed into the given cell
+    *   @param c the cell in which the worker need to be placed
+    */
     public void moveWorker(Cell c){
         cell.fireWorker();
         cell = c;
