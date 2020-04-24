7bdcb624aed21a59d7ad387570ea57576bb9bdfe
Marco <23193188+Marco1097@users.noreply.github.com>
2020-04-24 16:31:52 +0200

    Create GodTest.java

---
+++ src/test/java/it/polimi/ingsw/PSP23/model/GodTest.java
@@ -0,0 +1,12 @@
+package it.polimi.ingsw.PSP23.model;
+
+import it.polimi.ingsw.PSP23.model.*;
+import org.junit.*;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class GodTest {
+
+}
