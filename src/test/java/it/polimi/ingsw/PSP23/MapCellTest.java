78304133844e8d394fac9b359e6fe09436be36bd
Marco <23193188+Marco1097@users.noreply.github.com>
2020-04-24 10:27:51 +0200

    Update MapCellTest.java
    Test remaning isNear condition

--- src/test/java/it/polimi/ingsw/PSP23/MapCellTest.java
+++ src/test/java/it/polimi/ingsw/PSP23/MapCellTest.java
@@ -85,7 +85,15 @@ public class MapCellTest {
         Worker worker = new Worker(map.getCell(1, 1), Color.BLUE);
         assertTrue(map.getCell(1, 1).isOccupied());
         assertFalse(map.getCell(2, 2).isOccupied());
-        assertTrue(map.getCell(1, 1).isNear(map.getCell(1, 1), worker));
+        assertTrue(map.getCell(2, 2).isNear(map.getCell(2, 2), worker));
+        map.getCell(2, 2).build(Status.BUILT);
+        map.getCell(2, 2).build(Status.BUILT);
+        assertFalse(map.getCell(2, 2).isNear(map.getCell(2, 2), worker));
+        assertNull(map.getCell(6, 6));
+        assertEquals(map.getCell(1, 1).getWorker(), worker);
+        assertFalse(map.getCell(3, 1).isNear(map.getCell(3, 3), worker));
+        assertFalse(map.getCell(1, 3).isNear(map.getCell(3, 3), worker));
+
     }
 
 }
